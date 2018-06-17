9544e6dd42f875a1b67bc138675b6fe15f6dd59f
Richard Wells <rwells1703@gmail.com>
2018-06-17 14:12:03 +0100

    Takes input command line arguments instead of input()

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 # iOS-Icon-Grabber
 
-A basic python 3 script to download an app icon from the Apple App Store as a 512x512 jpg image.
-
-Run the script either in IDLE or on the command line using: "python iOS-Icon-Grabber.py"
+A basic Python3 script to download an app icon from the Apple App Store as a 512x512 jpg image.
 
 To get the app id for an app, visit the itunes website on a computer and find the app you want the icon for. The URL should resemble the following:
 ```
 https://itunes.apple.com/us/app/<APPNAME>/id<APPID>?mt=8
 ```
 
-This script takes the app id part of the URL as it's input.
+Execute the script from the command line using the following arguments:
+```
+python iOS-Icon-Grabber.py <APPID> <*NAMESCHEME>
+```
+
+Arguments with a * in front are optional.
+
+There are currently two possible nameschemes:
+1. '@appId' which uses the app id as the filename (this is default if you do not specify)
+2. '@trackName' which uses the app name as the filename
+
+Here is an example of using the script to download the icon for Google Photos and using the apps name as the filename:
+```
+python iOS-Icon-Grabber.py 962194608 @trackName
+```
 
 Enjoy!
--- iOS-Icon-Grabber.py
+++ iOS-Icon-Grabber.py
@@ -1,4 +1,7 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from sys import argv
 
 #Trims data from the start and end of a string
 def trim(data, search, direction):
@@ -13,26 +16,62 @@ def trim(data, search, direction):
     return data
 
 #Function to download an app icon
-def downloadIcon():
-    #Ask for the app id
-    appId = input("Enter app id: ")
+def downloadIcon(appId, nameScheme):
+    try:
     #Contact apple servers and retrieve the info document for the app
-    data = urlopen(("https://itunes.apple.com/lookup?id="+appId)).read().decode('utf-8')
+        data = urlopen(("https://itunes.apple.com/lookup?id="+appId)).read().decode('utf-8')
 
-    #Trim text from the start the image url
-    data = trim(data,'artworkUrl512":"',"forward")
-    #Trim text from the end the image url
-    data = trim(data,".jpg","backward")
+        #Trim text from the start the image url
+        image_url = trim(data,'artworkUrl512":"',"forward")
+        #Trim text from the end the image url
+        image_url = trim(image_url,'.jpg',"backward")
 
-    #Open the image file
-    image = urlopen(data)
+        #Open the image file
+        image = urlopen(image_url)
+        
+    except (HTTPError,URLError) as error:
+        #If urlopen cannot get data from that URL, return an error
+        print(error)
+        return
+
+    #Give the file the correct name
+    #Use the app id for the filename (this is default)
+    if nameScheme == "@appId":
+        name = appId
+
+    #Use the app name for the filename
+    elif nameScheme == "@trackName":
+        #Trim text from the start the image url
+        name = trim(data,'trackName":"',"forward")
+        #Trim text from the end the image url
+        name = trim(name,'"',"backward")[:-1]
+        
+    #Otherwise, use the custom name passed in   
+    else: 
+        name = nameScheme
+        
     #Save the image file
-    output = open((appId+".jpg"),"wb")
+    output = open((name+".jpg"),"wb")
     output.write(image.read())
     output.close()
 
     #Output a sucess message
-    print("'"+appId+".jpg' saved\n")
+    print("'"+name+".jpg' saved\n")
 
-while True:
-    downloadIcon()
+def main():
+    #Check if an app id has been passed in, if not return error
+    try:
+        appId = argv[1]
+    except IndexError:
+        print("Error: Need to pass in the app id as an argument")
+        return
+
+    #Check if a nameScheme has been passed in, if not use @appId
+    try:
+        nameScheme = argv[2]
+        downloadIcon(appId,nameScheme)
+    except:
+        downloadIcon(appId,"@appId")
+
+main()   
+        
