2322acd918b0bb4cd38fadf1980b0afb25061f5f
Richard Wells <rwells1703@gmail.com>
2018-06-18 18:26:10 +0100

    Changed the way command line arguments work

--- README.md
+++ README.md
@@ -9,18 +9,17 @@ https://itunes.apple.com/us/app/<APPNAME>/id<APPID>?mt=8
 
 Execute the script from the command line using the following arguments:
 ```
-python iOS-Icon-Grabber.py <APPID> <*NAMESCHEME>
+python iOS-Icon-Grabber.py -app_id <yourappid> [-file_name <yourfilename>]
 ```
 
-Arguments with a * in front are optional.
-
-There are currently two possible nameschemes:
-1. '@appId' which uses the app id as the filename (this is default if you do not specify)
-2. '@trackName' which uses the app name as the filename
+There are currently 3 possible file name schemes:
+1. '{app_id}' which uses the app id as the filename (this is default if you do not specify).
+2. '{app_name}' which uses the app name as the filename.
+2. 'your custom name here' which uses what ever you pass in as the file name (be careful because if you download multiple icons, the old one will be overwritten).
 
 Here is an example of using the script to download the icon for Google Photos and using the apps name as the filename:
 ```
-python iOS-Icon-Grabber.py 962194608 @trackName
+python iOS-Icon-Grabber.py -app_id 962194608 -file_name {app_name}
 ```
 
 Enjoy!
--- iOS-Icon-Grabber.py
+++ iOS-Icon-Grabber.py
@@ -1,6 +1,5 @@
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from urllib.error import URLError
+import urllib.request
+import urllib.error
 from sys import argv
 
 #Trims data from the start and end of a string
@@ -16,62 +15,75 @@ def trim(data, search, direction):
     return data
 
 #Function to download an app icon
-def downloadIcon(appId, nameScheme):
+def downloadIcon(app_id, file_name):
+    if app_id == "":
+        print("You have not entered an app id?")
+        return
     try:
-    #Contact apple servers and retrieve the info document for the app
-        data = urlopen(("https://itunes.apple.com/lookup?id="+appId)).read().decode('utf-8')
+
+        #Connect to itunes and retrieve the info document for the app
+        data = urllib.request.urlopen(("https://itunes.apple.com/lookup?id="+app_id)).read().decode('utf-8')
 
         #Trim text from the start the image url
         image_url = trim(data,'artworkUrl512":"',"forward")
         #Trim text from the end the image url
         image_url = trim(image_url,'.jpg',"backward")
-
-        #Open the image file
-        image = urlopen(image_url)
         
-    except (HTTPError,URLError) as error:
-        #If urlopen cannot get data from that URL, return an error
-        print(error)
+        #Open the image file
+        image = urllib.request.urlopen(image_url)
+
+    #If urlopen cannot get data from that URL, return an error
+    except (urllib.error.HTTPError,urllib.error.URLError) as error:
+        print("Invalid app id?")
         return
 
-    #Give the file the correct name
-    #Use the app id for the filename (this is default)
-    if nameScheme == "@appId":
-        name = appId
+    #Give the output file a name
+    #Use the app id for the file name (this is default)
+    if file_name == "{app_id}" or file_name == "":
+        file_name = app_id
 
     #Use the app name for the filename
-    elif nameScheme == "@trackName":
+    elif file_name == "{app_name}":
         #Trim text from the start the image url
-        name = trim(data,'trackName":"',"forward")
+        file_name = trim(data,'trackName":"',"forward")
         #Trim text from the end the image url
-        name = trim(name,'"',"backward")[:-1]
+        file_name = trim(file_name,'"',"backward")[:-1]
         
-    #Otherwise, use the custom name passed in   
-    else: 
-        name = nameScheme
-        
+    #Otherwise, use the custom name passed in
+
     #Save the image file
-    output = open((name+".jpg"),"wb")
+    output = open((file_name+".jpg"),"wb")
     output.write(image.read())
     output.close()
 
     #Output a sucess message
-    print("'"+name+".jpg' saved\n")
+    print("'"+file_name+".jpg' saved")
 
 def main():
-    #Check if an app id has been passed in, if not return error
-    try:
-        appId = argv[1]
-    except IndexError:
-        print("Error: Need to pass in the app id as an argument")
-        return
+    app_id = ""
+    file_name = ""
 
-    #Check if a nameScheme has been passed in, if not use @appId
-    try:
-        nameScheme = argv[2]
-        downloadIcon(appId,nameScheme)
-    except:
-        downloadIcon(appId,"@appId")
+    #Loop through all command line arguments apart from the file name
+    pos = 1
+    while pos < len(argv):
+        if argv[pos] == "-app_id":
+            if not pos == len(argv) - 1:
+                app_id = argv[pos+1]
+                pos += 1
+                
+        elif argv[pos] == "-file_name":
+            if not pos == len(argv) - 1:
+                file_name = argv[pos+1]
+                pos += 1
+            else:
+                print("You haven't specified the file name")
+
+        #Handles unknown arguments
+        else:
+            print("unrecognised argument: " + argv[pos])
+        pos += 1
+
+    downloadIcon(app_id,file_name)
 
 main()   
         
